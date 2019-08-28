360508c806e9d19390a67b8d160e20bc5175f1d8
Edoardo Mazzucchielli <mazzucchielli.edoardo@gmail.com>
2019-08-28 17:50:06 +0200

    Completamento DragDrop con animazioni, rimozione e completamento attività

--- todolist/src/app/lista/lista.component.ts
+++ todolist/src/app/lista/lista.component.ts
@@ -29,7 +29,6 @@ import { CdkDragMove, CdkDragRelease } from '@angular/cdk/drag-drop';
     transition('* => *', [
       query(':leave', [
         group([
-          animate('0.2s ease', style({ transform: 'translate(100%)' })),
           animate('0.5s 0.2s ease', style({ opacity: 0 }))
         ])
       ], { optional: true }),
@@ -44,15 +43,13 @@ import { CdkDragMove, CdkDragRelease } from '@angular/cdk/drag-drop';
 ]
 })
 export class ListaComponent implements OnInit {
-  @ViewChild('coso', {static: false}) el:ElementRef;
+  @ViewChild('coso', {static: false}) containerBounding:ElementRef;
   @ViewChild('at', {static: false}) at:ElementRef;
   arrayAttivita: Attivita[] = [
     { id: 0, titolo: 'Fare la spesa', descrizione: 'Fai la spesa entro domani'},
     { id: 1, titolo: 'Lavare i panni', descrizione: 'Lava i panni con ammorbidente'}
   ];
-  verificaSpostamento = true;
-  constructor(private renderer: Renderer2) { }
-
+  constructor() { }
   ngOnInit() {
   }
 
@@ -64,11 +61,25 @@ export class ListaComponent implements OnInit {
     indiceArray = this.arrayAttivita.indexOf(attivita);
     this.arrayAttivita.splice(indiceArray, 1);
   }
-  movimentoAttivita(event : CdkDragRelease<string[]>) {
-    var limiteX = this.el.nativeElement.getBoundingClientRect().left;
+  completaAttivita(attivita : Attivita): void{
+    var indiceArray;
+    indiceArray = this.arrayAttivita.indexOf(attivita);
+    this.arrayAttivita.splice(indiceArray, 1);
+  }
+  movimentoAttivita(event) {
+    var limiteX = this.containerBounding.nativeElement.getBoundingClientRect().left;
     var attivitaX = this.at.nativeElement.getBoundingClientRect().left;
-    var prova = event.source.element;
-    console.log(event);
-    //console.log(limiteX - (attivitaX + event.distance.x));
+  }
+  rilascioAttivita(event, attivita){
+    var limiteSx = this.containerBounding.nativeElement.getBoundingClientRect().left;
+    var limiteDx = this.containerBounding.nativeElement.getBoundingClientRect().right;
+    var attivitaSx = event.source.getRootElement().getBoundingClientRect().left;
+    var attivitaDx = event.source.getRootElement().getBoundingClientRect().right;
+    if(attivitaSx - 10 <= limiteSx)  this.completaAttivita(attivita);
+    else if(attivitaDx + 10 >= limiteDx)  this.rimuoviAttivita(attivita);
+    else event.source._dragRef.reset();
+  }
+  coloraSfondo(event){
+    
   }
 }
