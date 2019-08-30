5256cb25ee01caadc0f620058d022d35792463ce
Edoardo Grassi <edo.g@hotmail.it>
2019-08-30 18:04:47 +0200

    Implementazione Token Auth su backend

--- todolistapi/todolist/urls.py
+++ todolistapi/todolist/urls.py
@@ -13,18 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 from todolist.api import views
+
 
 router = routers.DefaultRouter()
 router.register(r'attivita', views.AttivitaViewSet)
 router.register(r'utente', views.UtenteViewSet)
+#router.register(r'^api-token-auth/', obtain_auth_token)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', obtain_auth_token)
 ]
